f6dc49791d634fe3bd3af67bd321300342f4ef28
shali <shalip91@gmail.com>
2023-03-22 14:58:27 +0200

    create project

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from sneaker_seeker import utils
+
+
+def main() -> None:
+    utils.print_utils()
+
+
+if __name__ == "__main__":
+    main()
---
+++ app/sneaker_seeker/utils.py
@@ -0,0 +1,3 @@
+
+def print_utils():
+    print("this is utils")
