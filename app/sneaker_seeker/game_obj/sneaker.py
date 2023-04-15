480d1fd6c45be06ae9649b4c774d561ab1271f61
shali persy <shalip91@gmail.com>
2023-04-15 03:20:28 +0300

    sneaker states (UNDETECTED, DETECTED, DESTROYED)

--- app/sneaker_seeker/game_obj/sneaker.py
+++ app/sneaker_seeker/game_obj/sneaker.py
@@ -1,3 +1,5 @@
+from enum import Enum, auto
+
 import matplotlib
 
 from sneaker_seeker.game_obj.player import Player
@@ -5,8 +7,27 @@ from sneaker_seeker.common_types import Speed
 
 
 class Sneaker(Player):
+    class State(Enum):
+        UNDETECTED = auto()
+        DETECTED = auto()
+        DESTROYED = auto()
 
     def __init__(self, location: dict, physical_specs: dict, direction: float = None,
                  los: float = 100, fov: float = 180, speed: Speed = None) -> None:
         super().__init__(location, physical_specs, direction, los, fov, speed)
-        self.detected = False
+        self.state = Sneaker.State.UNDETECTED
+
+    def detect(self):
+        self.state = Sneaker.State.DETECTED
+
+    def destroy(self):
+        self.state = Sneaker.State.DESTROYED
+
+    def is_undetected(self):
+        return self.state == Sneaker.State.UNDETECTED
+
+    def is_detected(self):
+        return self.state == Sneaker.State.DETECTED
+
+    def is_destroyed(self):
+        return self.state == Sneaker.State.DESTROYED
