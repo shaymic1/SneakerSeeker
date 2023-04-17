7a2c1c81a2048dcf1494b41d823712e59d0f4ce6
shali persy <shalip91@gmail.com>
2023-04-17 05:47:13 +0300

    split the the common_type.py into python package

---
+++ app/sneaker_seeker/common_types/__init__.py
@@ -0,0 +1,3 @@
+from .location import Location
+from .physical_specs import PhysicalSpecs
+from .speed_vec import SpeedVec
---
+++ app/sneaker_seeker/common_types/physical_specs.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class PhysicalSpecs:
+    cruise_speed: float = 55
+    max_speed: float = 100
+    max_speed_time: float = 0
