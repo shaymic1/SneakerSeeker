3d3ca218e1dbeedfc210530d1605560c339065d7
shali persy <shalip91@gmail.com>
2023-04-18 18:38:58 +0300

    fix the speed setting method for "PathPlannerRandomWalk

--- app/sneaker_seeker/path_planner/path_planner_random_walk.py
+++ app/sneaker_seeker/path_planner/path_planner_random_walk.py
@@ -10,5 +10,5 @@ class PathPlannerRandomWalk(PathPlanner):
         self.roi = roi
 
     def set_path(self, player: Player):
-        player.speed.vx = np.random.uniform(low=0, high=player.physical_specs.cruise_speed)
-        player.speed.vy = player.physical_specs.cruise_speed - player.speed.vx
+        player.speed.magnitude = np.random.uniform(low=0, high=player.physical_specs.cruise_speed)
+        player.speed.direction = np.random.uniform(low=0, high=np.pi*2)
