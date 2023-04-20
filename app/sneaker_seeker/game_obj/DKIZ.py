5ae136aa1d1f1f213f161af6d732558d8b30d5ca
shali persy <shalip91@gmail.com>
2023-04-20 22:06:07 +0300

    Optimize the canvas object making.

--- app/sneaker_seeker/game_obj/DKIZ.py
+++ app/sneaker_seeker/game_obj/DKIZ.py
@@ -7,15 +7,12 @@ from .movable import Movable
 class DKIZ(Movable):
     """this is the Dynamic Keep-in Zone of the sneakers"""
 
-    __shapes = {"circle": Circle}
 
     def __init__(self, shape: dict, location: dict = None, speed: dict = None, **args: dict) -> None:
         super().__init__(location, speed)
         self.type: str = shape["type"]
         self.dimensions: dict = shape[self.type]
         self.max_dist_from_center = max(self.dimensions.values())
-        self.shape: matplotlib.patches = DKIZ.__shapes[self.type](**args["appearance"], **shape[self.type],
-                                                                  xy=(location["x"], location["y"]))
 
     def contains(self, other_location: Point2D):
         if self.type == 'circle':
